5b6d48ca8eefc22095c1eb1b16b0fbedda8a3fc8
Willice Opalo <williceopalo11@gmail.com>
2026-06-29 13:19:13 +0300

    authTest

--- app/context/auth-context.tsx
+++ app/context/auth-context.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-
 import { createContext, useContext, useEffect, useState } from "react";
 import { supabase } from "@/lib/supabase";
 import type { Session, User } from "@supabase/supabase-js";
